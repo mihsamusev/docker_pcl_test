73d9ea7887bab76a0b32b4679fce0826229d682a
Mihhail <mih.samusev@gmail.com>
2019-09-12 15:00:26 +0200

    specified os in .sh script name

---
+++ start_pcl_win.sh
@@ -0,0 +1,7 @@
+docker run \
+    --name pcl_container \
+    -it \
+    --rm \
+    -e DISPLAY=host.docker.internal:0 \
+    --mount type=bind,src=$(pwd)\\cloud_viewer,dst=/code \
+    innerspace/docker-pcl:ubuntu_16.04

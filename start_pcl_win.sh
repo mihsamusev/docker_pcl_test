3b335a6b41028ca6ccd67b2c5d3d45baa94b4fe0
mihsamusev <40422737+mihsamusev@users.noreply.github.com>
2019-09-12 15:45:41 +0200

    delet

--- start_pcl_win.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    --name pcl_container \
-    -it \
-    --rm \
-    -e DISPLAY=host.docker.internal:0 \
-    --mount type=bind,src=$(pwd)\\cloud_viewer,dst=/code \
-    innerspace/docker-pcl:ubuntu_16.04
